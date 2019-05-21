d5eaa109459ddb4bec8f01e746a3a3a82d9aff2f
Samantha brake <sammy.brake@gmail.com>
2019-05-21 17:49:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, arg) {
   return fn.apply(thisValue, [arg]);
 };
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
